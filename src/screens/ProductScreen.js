85a6b8bf202167ab97eb742ddb3587d58749b5ce
HARSHSINGH0 <hs475052@gmail.com>
2021-06-15 16:08:06 +0530

    added all the files

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap"
 import Rating from "../components/Rating"
-import products from "../products"
-
+// import products from "../products"
+import axios from "axios"
 function ProductScreen({ match }) {
-  const product = products.find((p) => p._id == match.params.id)
+  const [product, setProduct] = useState([])
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [])
   return (
     <div>
       <Link to="/" className="btn btn-secondary my-3">
@@ -51,7 +58,11 @@ function ProductScreen({ match }) {
                 <Row>
                   <Col>Status:</Col>
                   <Col>
-                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    {product.countInStock > 0 && product.countInStock < 6
+                      ? product.countInStock + " item left hurry!!"
+                      : product.countInStock > 0
+                      ? "In stock"
+                      : "Out of Stock"}
                   </Col>
                 </Row>
               </ListGroup.Item>
